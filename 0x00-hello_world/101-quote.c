fc6c92dd78fe7616779d5d6c65464d38c1b8835f
Uche felix chukwudi <felixuc2580@gmail.com>
2022-03-14 09:18:01 +0100

    update 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,20 +1,14 @@
-#nclude < stdio.h >
+#include <stdio.h>
 
 /**
-* main - Entry point
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, ");
+	fprintf(stderr, "2015-10-19\n");
 
-		fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, ");
-
-			fprintf(stderr, "2015-10-19\n");
-
-
-
-				return (1);
-
+	return (1);
 }
-
-
