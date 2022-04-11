9a1a6a50b598067c6cf4f375a24d531ab3dd864e
Uche felix chukwudi <felixuc2580@gmail.com>
2022-04-11 17:16:07 +0100

    preprocessors

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,48 +1,54 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * string_nconcat - function to concatnate strings with n bytes
- * @s1: destination for concatnation
- * @s2: source of string
- * @n: int type for size of byte
- * Return: pointer to new memory allocated
+ * string_nconcat - ...
+ * @s1: ...
+ * @s2: ...
+ * @n: ...
+ *
+ * Return: ...
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-  int count, count1;
-  int sign = n;
-  char *ptr;
-  int len1, len2;
+	unsigned int i = 0, j = 0, k = 0, l = 0;
+	char *str;
 
-  if (s1 == NULL)
-    s1 = "";
-  if (s2 == NULL)
-    s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-  for (len1 = 0; s1[len1] != '\0'; len1++)
-    ;
-  for (len2 = 0; s2[len2] != '\0'; len2++)
-    ;
+	while (s1[i])
+		i++;
 
-  if (sign >= len2)
-    {
-      sign = len2;
-      ptr = malloc(sizeof(char) * (len1 + len2 + 1));
-    }
-  else
-    ptr = malloc(sizeof(char) * (len1 + n + 1));
-  if (ptr == NULL)
-    return (NULL);
-  for (count = 0; count < len1; count++)
-    {
-      ptr[count] = s1[count];
-    }
-  for (count1 = 0; count1 < sign; count1++)
-    {
-      ptr[count++] = s2[count1];
-    }
-  ptr[count++] = '\0';
-  return (ptr);
+	while (s2[k])
+		k++;
+
+	if (n >= k)
+		l = i + k;
+	else
+		l = i + n;
+
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
+		return (NULL);
+
+	k = 0;
+	while (j < l)
+	{
+		if (j <= i)
+			str[j] = s1[j];
+
+		if (j >= i)
+		{
+			str[j] = s2[k];
+			k++;
+		}
+		j++;
+	}
+	str[j] = '\0';
+	return (str);
 }
